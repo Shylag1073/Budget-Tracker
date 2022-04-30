008b7da9d447bca101e2ea68023b6c7375bb26bf
shylag1073 <shylag1073@gmail.com>
2022-04-30 15:55:14 -0500

    ahhhh

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+// "mongodb+srv://Shylar:Password@cluster0.8ivwd.mongodb.net/socialmedia?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
