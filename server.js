4eb9930d94e8ebd8a693989214afd934d15be357
shylag1073 <shylag1073@gmail.com>
2022-05-21 18:50:30 -0500

    mongo actually worksgit add .git add .git add .

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || PORT, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
